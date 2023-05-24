1a31e674d736b47a15287f94e83bde1417b44625
wewewe08 <yvonnehuang0810@gmail.com>
2023-05-24 17:01:29 -0400

    Added a C program file that uses 'puts' function to print statements

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * Making a C program that prints a statement
+ * using the 'puts' function
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
