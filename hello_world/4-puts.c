3fa600e0860773ea59e569411659d3e311b5f38d
wewewe08 <yvonnehuang0810@gmail.com>
2023-05-24 17:17:51 -0400

    Added description for main

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  * 
- * Description: '\"Programming is like building a multilingual puzzle'
+ * Description: 'Using the puts function to print statements'
  * 
  * Return: Always 0 (Success)
  */
