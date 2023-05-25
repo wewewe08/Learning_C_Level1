b569645f1f2b7355c6c43620947b3c57ed2711db
wewewe08 <yvonnehuang0810@gmail.com>
2023-05-24 22:26:09 -0400

    fixed printf formatting

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 
 	if (last_num > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, last_num);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_num);
 	}
 	else if (last_num == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, last_num);
+		printf("Last digit of %d is %d and is 0\n", n, last_num);
 	}
 	else if (last_num < 6 && last_num != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, last_num);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_num);
 	}
 
 	return (0);
