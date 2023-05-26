721ba4cf4ad0a4a0ddb71b4bf95e880ef407546c
wewewe08 <yvonnehuang0810@gmail.com>
2023-05-26 18:20:52 -0400

    Printing numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print all single digit base10 numbers
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
