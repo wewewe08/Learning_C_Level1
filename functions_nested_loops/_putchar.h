920432c07d06d906301b2b73b0ea33b2aad8ab13
wewewe08 <yvonnehuang0810@gmail.com>
2023-05-27 20:28:58 -0400

    Added comments in header file

--- functions_nested_loops/_putchar.h
+++ functions_nested_loops/_putchar.h
@@ -1,6 +1,10 @@
+/* This is an 'include guard'
+ * prevents file from being included twice
+ */
 #ifndef _PUT_CHAR_H
 #define _PUT_CHAR_H
 
+/* lets compiler know that this function exists somewhere  */
 void _putchar(char c);
 
 #endif
