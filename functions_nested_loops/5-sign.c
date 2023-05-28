6f270d3b0ec6f046bea9421ae6926f1db2502635
wewewe08 <yvonnehuang0810@gmail.com>
2023-05-28 15:48:13 -0400

    Checking for integer signs

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ * @c: integer value
+ *
+ * Description: check for integer sign
+ *
+ * Return: 1, 0, or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		/* positive number */
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		/* is zero */
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		/* negative number */
+		_putchar('-');
+		return (-1);
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -15,4 +15,6 @@ int _islower(int c);
 
 int _isalpha(int c);
 
+int print_sign(int n);
+
 #endif
