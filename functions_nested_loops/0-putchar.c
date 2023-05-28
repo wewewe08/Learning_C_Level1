0534f5b4cbef6657a39cc52c0939058a4593a386
wewewe08 <yvonnehuang0810@gmail.com>
2023-05-27 20:27:15 -0400

    Print '_putchar' using functions

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "_putchar.h"
 
 /**
  * main - Entry point
@@ -8,6 +9,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
---
+++ functions_nested_loops/_putchar.h
@@ -0,0 +1,6 @@
+#ifndef _PUT_CHAR_H
+#define _PUT_CHAR_H
+
+void _putchar(char c);
+
+#endif
