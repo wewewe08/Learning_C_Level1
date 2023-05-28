1766585b2748ea9c686f5916d87c85dd2bc4c9b1
wewewe08 <yvonnehuang0810@gmail.com>
2023-05-28 15:58:15 -0400

    Added question 6 files

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ * @n: integer value
+ *
+ * Description: check for absolute value of integer
+ *
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	
+}
