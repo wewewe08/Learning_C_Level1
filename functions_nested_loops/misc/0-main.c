00b32f01014b74a94686084c0154fdd65e64811a
wewewe08 <yvonnehuang0810@gmail.com>
2023-05-30 17:44:20 -0400

    Function to find the largest number within 4 digits

--- functions_nested_loops/misc/0-main.c
+++ functions_nested_loops/misc/0-main.c
@@ -6,7 +6,7 @@ int main(void)
 	int num1, num2, num3, num4;
 
 	printf("Enter 4 numbers separated by spaces: ");
-	scanf("%d %d %d %d", num1, num2, num3, num4);
+	scanf("%d %d %d %d", &num1, &num2, &num3, &num4);
 
 	max_num(num1, num2, num3, num4);
 	return(0);
