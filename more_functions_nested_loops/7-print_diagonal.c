c3cde1ce63d06087930ce77d0754857f235b4c14
wewewe08 <yvonnehuang0810@gmail.com>
2023-05-31 21:59:43 -0400

    draw a diagonal line on the terminal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - entry point
+ * @n: number of times the \ should be printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int start_num, spaces;
+
+	if (n == 0 || n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (start_num = 0; start_num < n; start_num++)
+		{
+			for (spaces = 0; spaces < start_num; spaces++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
