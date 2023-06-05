536c244d92460f912b97a249a9fc44f963cdec62
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-05 19:33:43 -0400

    Tried new test cases

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -19,14 +19,14 @@ void puts_half(char *str)
 	{
 		char ch = str[index];
 
-		if (counter % 2 == 1)
+		if (counter % 2 == 1) /* checks for odd number of total characters */
 		{
-			if (index <= ((counter - 1) / 2))
+			if (index <= ((counter - 1) / 2)) /* ignoring first half */
 			{
 				continue;
 			}
 		}
-		else if (index < (counter / 2)) /* first half of string */
+		else if (index < (counter / 2)) /* even number of total characters, ignoring first half */
 		{
 			continue;
 		}
