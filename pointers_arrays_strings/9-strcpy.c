7491ad85010305728e1d71c85a290d38242682bb
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-05 20:46:42 -0400

    copy strings using pointers

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * strcpy - entry point
+ * @*dest: destination string
+ * @*src: the string to copy
+ * Return: char pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index;
+
+	for (index = 0; src[index] != '\0'; index++)
+	{
+		dest[index] = src[index];
+	}
+	return (dest);
+}
