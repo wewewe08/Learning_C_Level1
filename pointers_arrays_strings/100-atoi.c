fd4aea16e77648903cbdc515dc1be7a39ce2ba8a
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-05 21:27:00 -0400

    convert string to integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,12 +8,31 @@
 
 int _atoi(char *s)
 {
-	int index, result = 0;
+	int index, result = 0, sign = 0;
 
 	for (index = 0; s[index] != '\0'; index++)
 	{
-		if (str[index] ==
-		result = result + s[index] - '0';
+		/* check if character is a number */
+		if (s[index] >= 48 && s[index] <= 57)
+		{
+			/* result * 10 is to shift the numbers down one place
+			 * ex/ '98' -> (0 * 10) + ('9' - '0') = 9
+			 * the ascii value of 9 minus ascii value of 0 = the integer 9
+			 * next number is 8 -> (9 * 10) + ('8' - '0') = 90 + 8 = 98
+			 */
+			result = (result * 10) + (s[index] - '0');
+			
+			/* checking if the previous character was a '-' for negative numbers */
+			if (s[index - 1] == '-')
+			{
+				sign = -1;
+			}
+		}
+	}
+
+	if (sign != 0)
+	{
+		result = result * sign;
 	}
 	return (result);
 }
