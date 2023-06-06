d88f99d6686f8df37968cead41559481826a1a31
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-05 21:50:00 -0400

    string to integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,31 +8,39 @@
 
 int _atoi(char *s)
 {
-	int index, result = 0, sign = 0;
+	int index, result = 0, positive = 0, negative = 0;
 
 	for (index = 0; s[index] != '\0'; index++)
 	{
+		/* counting amount of signs */
+		if (s[index] == '+')
+		{
+			positive++;
+		}
+		else if (s[index] == '-')
+		{
+			negative++;
+		}
+
 		/* check if character is a number */
 		if (s[index] >= 48 && s[index] <= 57)
 		{
 			/* result * 10 is to shift the numbers down one place
-			 * ex/ '98' -> (0 * 10) + ('9' - '0') = 9
-			 * the ascii value of 9 minus ascii value of 0 = the integer 9
-			 * next number is 8 -> (9 * 10) + ('8' - '0') = 90 + 8 = 98
+			 * s[index] - '0' substracts the ascii values to get the actual integer
+			 * ex/ '9' - '0' would be 57 - 48, which is 9 as an integer
 			 */
 			result = (result * 10) + (s[index] - '0');
-			
-			/* checking if the previous character was a '-' for negative numbers */
-			if (s[index - 1] == '-')
+			if (s[index + 1] < 48 || s[index + 1] > 57)
 			{
-				sign = -1;
+				break;
 			}
 		}
 	}
 
-	if (sign != 0)
+	/* multiply by -1 to make integer negative */
+	if ((positive == 0 && negative % 2 == 1) || (positive > 0 && negative % 2 == 1))
 	{
-		result = result * sign;
+		result = result * -1;
 	}
 	return (result);
 }
