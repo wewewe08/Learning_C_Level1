a5b8f05e1bdbcc388e50d7b8dffb2487a15c9f6e
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-07 22:59:53 -0400

    fixed up for loop formatting

--- more_pointers/0-strcat.c
+++ more_pointers/0-strcat.c
@@ -2,9 +2,9 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int dest_len = 0, src_len = 0;
-	int index, total_len;
+	int index, total_len, dest_len = 0, src_len = 0;
 
+	/* getting the lenghts of each string */
 	while (dest[dest_len] != '\0')
 	{
 		dest_len++;
@@ -15,14 +15,15 @@ char *_strcat(char *dest, char *src)
 		src_len++;
 	}
 
-	total_len = dest_len + src_len;
+	total_len = dest_len + src_len; /* adding lengths together to get the length of concatenated string */
 
+	/* add on src string starting from the terminating null byte of dest string */
 	for (index = dest_len, src_len = 0; index < total_len; index++, src_len++)
 	{
 		dest[index] = src[src_len];
 	}
 
-	dest[index] = '\0';
+	dest[index] = '\0'; /* add back terminating null byte */
 
 	return (dest);
 }
