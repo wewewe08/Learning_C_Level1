7daef48c95095d91e92059c7cd2cafad5350e2fc
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-10 21:05:04 -0400

    comparing strings

--- more_pointers/3-main.c
+++ more_pointers/3-main.c
@@ -5,9 +5,11 @@ int main(void)
 {
 	char s1[] = "Hello";
 	char s2[] = "World!";
+	char s3[] = "Hello, World!";
 
 	printf("%d\n", _strcmp(s1, s2));
 	printf("%d\n", _strcmp(s2, s1));
 	printf("%d\n", _strcmp(s1, s1));
+	printf("%d\n", _strcmp(s1, s3));
 	return (0);
 }
--- more_pointers/3-strcmp.c
+++ more_pointers/3-strcmp.c
@@ -2,13 +2,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int index;
+	int count;
 
-	for (index = 0; s1[index] != '\0' && s2[index] != '\0'; index++)
+	for (count = 0; s1[count] != '\0' && s2[count] != '\0'; count++)
 	{
-		if (s1[index] != s2[index])
+		if (s1[count] != s2[count])
 		{
-			return (s1[index] - s2[index]);
+			return (s1[count] - s2[count]);
 		}
 	}
 
