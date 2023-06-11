c953c5697b7d451148cbfdb7f498a502e5102f29
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-10 21:13:08 -0400

    added comments

--- more_pointers/3-strcmp.c
+++ more_pointers/3-strcmp.c
@@ -2,13 +2,19 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int count;
+	int s1_index = 0, s2_index;
 
-	for (count = 0; s1[count] != '\0' && s2[count] != '\0'; count++)
+	for (s2_index = 0; s2[s2_index] != '\0'; s2_index++)
 	{
-		if (s1[count] != s2[count])
+		if (s1[s1_index] != s2[s2_index])
 		{
-			return (s1[count] - s2[count]);
+			return (s1[s1_index] - s2[s2_index]); /* gets difference between the two char ASCII values */
+		}
+
+		/* stops s1_index at the terminating character while continuing s2_index */
+		if (s1[s1_index] != '\0')
+		{
+			s1_index++;
 		}
 	}
 
