193f866ae2540146f2a83854431d1b187839a304
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-10 20:56:14 -0400

    comparing strings

--- more_pointers/3-strcmp.c
+++ more_pointers/3-strcmp.c
@@ -2,13 +2,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int index1, index2;
+	int index;
 
-	for (index1 = 0, index2 = 0; s1[index1] != '\0' && s2[index2] != '\0'; index1++, index2++)
+	for (index = 0; s1[index] != '\0' && s2[index] != '\0'; index++)
 	{
-		if (s1[index1] != s2[index2])
+		if (s1[index] != s2[index])
 		{
-			return (s1[index1] - s2[index2]);
+			return (s1[index] - s2[index]);
 		}
 	}
 
