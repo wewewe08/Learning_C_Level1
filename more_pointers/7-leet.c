68293e8839bb791c08e788ea8fd735fd398ea12d
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-12 00:21:54 -0400

    adding comments

--- more_pointers/7-leet.c
+++ more_pointers/7-leet.c
@@ -3,16 +3,20 @@
 char *leet(char *c)
 {
 	int count, index;
+	
+	/* these arrays are for cross referencing each other since they will use the same index */
 	char letters[] = {'a', 'e', 'o', 't', 'l'};
 	char numbers[] = {'4','3','0','7','1'};
 
 	for (count = 0; c[count] != '\0'; count++)
 	{
+		/* loop over the letters array */
 		for (index = 0; index < 5; index++)
 		{
+			/* check if the string has the specified lowercase letter or uppercase letter */
 			if (c[count] == letters[index] || c[count] == letters[index] - 32)
 			{
-				c[count] = numbers[index];
+				c[count] = numbers[index]; /* set that element to the number in the numbers array */
 			}
 		}
 	}
