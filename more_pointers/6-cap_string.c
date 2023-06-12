9eed9273faaf17146a656f0f202eae5bfdd2b2c5
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-11 23:24:26 -0400

    capitalizing strings

--- more_pointers/6-cap_string.c
+++ more_pointers/6-cap_string.c
@@ -8,21 +8,23 @@ char *cap_string(char *c)
 	/* looping over string */
 	for (string_index = 0; c[string_index] != '\0'; string_index++)
 	{
-		/* loop over the array of separators */
-		for (arr_index = 0; arr_index < arr_len; arr_index++)
+		/* checks to see if the first character in the string is a lower case letter */
+		if (c[string_index] >= 'a' && c[string_index] <= 'z' && string_index == 0)
 		{
-			if (c[string_index + 1] >= 'a' && c[string_index + 1] <= 'z')
+			c[string_index] -= 32;
+			continue;
+		}
+
+		if (c[string_index + 1] >= 'a' && c[string_index + 1] <= 'z')
+		{
+			/* loop over the array of separators */
+			for (arr_index = 0; arr_index < arr_len; arr_index++)
 			{
 				/* if the separator is present in the string */
 				if (separators[arr_index] == c[string_index])
 				{
-					c[string_index + 1] -= 32; /* set the next char to upper case */
+					c[string_index + 1] -= 32; /* set the letter to upper case */
 					break; /* no need to check the rest of the array for this char, so break and move to next char in the string */
-				}
-				else if (string_index == 0)
-				{
-					c[string_index] -=  32;
-					break;
 				}
 			}
 		}
