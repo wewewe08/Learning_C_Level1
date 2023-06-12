d323ac0f107c82dc1402632321d6f01563bfe8bf
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-12 00:15:39 -0400

    convert string to leet

--- more_pointers/7-leet.c
+++ more_pointers/7-leet.c
@@ -2,11 +2,19 @@
 
 char *leet(char *c)
 {
-	int count;
+	int count, index;
+	char letters[] = {'a', 'e', 'o', 't', 'l'};
+	char numbers[] = {'4','3','0','7','1'};
 
 	for (count = 0; c[count] != '\0'; count++)
 	{
-		
+		for (index = 0; index < 5; index++)
+		{
+			if (c[count] == letters[index] || c[count] == letters[index] - 32)
+			{
+				c[count] = numbers[index];
+			}
+		}
 	}
 	return (c);
 }
