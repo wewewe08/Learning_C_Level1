3f845b0a7b8a75c47e9baceeaa29459f48d5b67b
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-11 21:09:04 -0400

    capitalize all words of a string

--- more_pointers/6-cap_string.c
+++ more_pointers/6-cap_string.c
@@ -5,13 +5,16 @@ char *cap_string(char *c)
 	int count, count2, len = 13;
 	char separators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
+	/* looping over string */
 	for (count = 0; c[count] != '\0'; count++)
 	{
+		/* loop over the array of separators */
 		for (count2 = 0; count2 < len; count2++)
 		{
+			/* if the separator is present in the string and the next char is a lower case letter */
 			if (separators[count2] == c[count] && (c[count + 1] >= 'a' && c[count + 1] <= 'z'))
 			{
-				c[count + 1] -= 32;
+				c[count + 1] -= 32; /* set the next char to upper case */
 			}
 		}
 	}
