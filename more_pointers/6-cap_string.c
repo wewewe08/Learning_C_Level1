3c2498e42ff38f0220d342b41220be2acf5d4485
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-11 21:14:42 -0400

    capitalize all words of a string

--- more_pointers/6-cap_string.c
+++ more_pointers/6-cap_string.c
@@ -15,6 +15,7 @@ char *cap_string(char *c)
 			if (separators[arr_index] == c[string_index] && (c[string_index + 1] >= 'a' && c[string_index + 1] <= 'z'))
 			{
 				c[string_index + 1] -= 32; /* set the next char to upper case */
+				break; /* no need to check the rest of the array for this char, so break and move to next char in the string */
 			}
 		}
 	}
