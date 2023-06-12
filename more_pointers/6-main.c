9a3b5be5982e466d8f40d85bcc9754cdbbda81f8
wewewe08 <yvonnehuang0810@gmail.com>
2023-06-11 23:05:22 -0400

    capitalizing strings

--- more_pointers/6-main.c
+++ more_pointers/6-main.c
@@ -4,10 +4,15 @@
 int main(void)
 {
 	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char str2[] = "hello world! expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
 	char *ptr;
+	char *ptr2;
 
 	ptr = cap_string(str);
+	ptr2 = cap_string(str2);
+
 	printf("%s",ptr);
 	printf("%s", str);
+	printf("%s", ptr2);
 	return (0);
 }
